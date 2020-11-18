128f7d997a2d89883ab6f56c4257e84da01b104d
Glitch (wjclub-telegram-bot-album-creator) <none>
2020-11-18 20:30:46 +0000

    💏🚖 Checkpoint ./watch.json:20002093/210 ./package.json:20002093/1271 ./albumMeBot.js:20002093/484

--- albumMeBot.js
+++ albumMeBot.js
@@ -1,3 +1,26 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
+}, 280000);
+
+///////////////// BOT CODE ////////////////////
+
 const Telegraf = require('telegraf')
 const TelegrafI18n = require('telegraf-i18n')
 const TelegrafLocalSession = require('telegraf-session-local')
